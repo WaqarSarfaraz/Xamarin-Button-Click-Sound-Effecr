c98d80e4c17d578fe01d89c5a519748f8b5dc3b2
waqarkhan <developer2.hrsts@gmail.com>
2019-08-09 02:38:02 -0700

    XAM.Plugin.AudioPlayer Example

---
+++ SoundEffect/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Plugin.SimpleAudioPlayer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SoundEffect
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+
+            //var assembly = typeof(App).GetTypeInfo().Assembly;
+            //Stream audioStream = assembly.GetManifestResourceStream("YourSharedAssemblyName." + "yoursound.wav");
+            //var alertSound = CrossSimpleAudioPlayer.CreateSimpleAudioPlayer();
+            //alertSound.Load(audioStream);
+            //alertSound.Play();
+            InitializeComponent();
+        }
+        Stream GetStreamFromFile(string filename)
+        {
+            var assembly = typeof(App).GetTypeInfo().Assembly;
+            var stream = assembly.GetManifestResourceStream("SoundEffect." + filename);
+
+            return stream;
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var player = CrossSimpleAudioPlayer.Current;
+            var folder = "Techno";
+            player.Load(GetStreamFromFile($"Audio.{folder}.clap.wav"));
+            player.Play();
+        }
+    }
+}
